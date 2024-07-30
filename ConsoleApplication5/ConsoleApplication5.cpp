ee5224331658ebe4ffe67afb58e74d843882ac26
Sasha Atutsasha <sasha2015to@gmail.com>
2024-07-30 13:44:19 +0300

    mistake done: add & in Average& operator+=

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -2,26 +2,27 @@
 
 class Average {
 private:
-  std::int32_t m_value;
-  std::int32_t m_count;
+    std::int32_t m_value{ 0 };
+    std::int32_t m_count{0};
 
 public:
-  Average() : m_value(0), m_count(0) {}
+  Average() {}
   Average(const Average &other) = default;
 
-  Average operator+=(const Average &value) {
+  Average& operator+=(const Average &value) {
     ++m_count;
     m_value += value.m_value;
     return *this;
   }
 
-  Average operator+=(int val) {
+  Average& operator+=(int val) {
     ++m_count;
     m_value += val;
     return *this;
   }
     
   friend std::ostream &operator<<(std::ostream &os, const Average &a) {
+    if (a.m_count == 0) return os << 0;
     os << (static_cast<double>(a.m_value) / a.m_count);
     return os;
   }
@@ -31,7 +32,7 @@ public:
 int main() {
   
   Average avg{};
-  //std::cout << avg << '\n';
+  std::cout << avg << '\n';
 
   avg += 4;
   std::cout << avg << '\n'; // 4 / 1 = 4
@@ -51,8 +52,8 @@ int main() {
   // после выхода из выражения  m_count=5 m_value=32
   std::cout << avg << '\n'; // (4 + 8 + 24 - 10 + 6 + 10) / 6 = 7
 
-  //Average copy{avg};
-  //std::cout << copy << '\n';
+  Average copy{avg};
+  std::cout << copy << '\n';
 
   return 0;
 }
