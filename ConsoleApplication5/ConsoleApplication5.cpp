d58a5afb4ed67388ff4f7ce23012df4004b7cc7d
Sasha HP <sasha2015to@gmail.com>
2024-08-12 08:40:35 +0300

    21.y — Chapter 21 project

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -1,230 +1,320 @@
+/*
+https://www.learncpp.com/cpp-tutorial/chapter-21-project/
+21.y — Chapter 21 project
+*/
+
+#include <array>
 #include <cassert>
 #include <iostream>
-#include <algorithm> // std::copy
-#include <cstddef> // std::size_t
+#include <limits>
+#include "Random.h"
+#include <conio.h>
 
-class dumb_array
+// Increase amount of new lines if your board isn't
+// at the very bottom of the console
+constexpr int g_consoleLines{ 25 };
+
+class Direction
 {
 public:
-    // 
-    dumb_array(std::size_t size = 0)
-        : mSize(size),
-        mArray(mSize ? new int[mSize]() : nullptr)
-    {}
-
-    // 
-    dumb_array(const dumb_array& other)
-        : mSize(other.mSize),
-        mArray(mSize ? new int[mSize] : nullptr)
+    enum Type
     {
-        std::copy(other.mArray, other.mArray + mSize, mArray);
+        up,
+        down,
+        left,
+        right,
+        maxDirections,
+    };
+
+    Direction(Type type)
+        :m_type(type)
+    {
+    }
+    Type getType() const
+    {
+        return m_type;
     }
 
-    ~dumb_array()
+    Direction operator-() const
     {
-        delete[] mArray;
-    }
-
-    dumb_array& operator=(const dumb_array& other)
-    {
-        if (this != &other)
+        switch (m_type)
         {
-            // избавл¤емс¤ от старых данных...
-            delete[] mArray; // (2)
-            mArray = nullptr; // (2) (i)
-
-            mSize = other.mSize; // (3)
-            mArray = mSize ? new int[mSize] : nullptr; // (3)
+        case up:    return Direction{ down };
+        case down:  return Direction{ up };
+        case left:  return Direction{ right };
+        case right: return Direction{ left };
+        default:    break;
         }
 
-        return *this;
+        assert(0 && "Unsupported direction was passed!");
+        return Direction{ up };
     }
 
-    dumb_array& operator=(dumb_array other) // (1)
+    static Direction getRandomDirection()
     {
-        swap(*this, other); // (2)
-
-        return *this;
-    }
-
-    dumb_array(dumb_array&& other) noexcept // (1)
-        : dumb_array() // 
-    {
-        swap(*this, other);
-    }
-
-    friend void swap(dumb_array& first, dumb_array& second) // nothrow
-    {
-        // 
-        using std::swap;
-
-        //
-        //
-        swap(first.mSize, second.mSize);
-        swap(first.mArray, second.mArray);
+        Type random{ static_cast<Type>(Random::get(0, Type::maxDirections - 1)) };
+        return Direction{ random };
     }
 
 private:
-    std::size_t mSize;
-    int* mArray;
+    Type m_type{};
 };
 
-/*
-MyString
-std::copy_n(str.m_data, m_length, m_data); // str.m_data -> m_data
-std::max
-<algorithm>
-MyString& <- return *this;
-operator<<
-char* m_data{}
-int m_length{}
-new char[static_cast<std::size_t>(length)];
-nullptr
-*/
-
-namespace mystring {
-
-    class MyString
+std::ostream& operator<<(std::ostream& stream, Direction dir)
+{
+    switch (dir.getType())
     {
-    private:
-        char* m_data{};
-        size_t m_length{};
-
-    public:
-        static int count;
-
-
-    public:
-        MyString(const char* data = nullptr, size_t length = 0)
-            : m_length{ length }
-        {
-            std::cout << "ctor\n";
-            if (length)
-            {
-                m_data = new char[length];
-                std::copy_n(data, length, m_data); // copy length elements of data into m_data
-            }
-        }
-
-        ~MyString()
-        {
-            ++count;
-            std::cout << "dctor " << count << '\n';
-            delete[] m_data;
-        }
-
-        MyString(const MyString& str) // some complilers (gcc) warn if have pointer members but no declared copy constructor
-        {
-
-            if (str.m_length)
-            {
-                m_length = str.m_length;
-                m_data = new char[m_length];
-                std::copy_n(str.m_data, m_length, m_data);
-            }
-        }
-
-
-        // Overloaded assignment
-        //MyString& operator=(const MyString& str)
-        //{
-        //    std::cout << "operator=(const MyString& str)\n";
-        //    if (this == &str)
-        //        return *this;
-
-        //    // if data exists in the current string, delete it
-        //    if (m_data) delete[] m_data;
-
-        //    m_length = str.m_length;
-        //    m_data = nullptr;
-
-        //    if (m_length)
-        //        m_data = new char[static_cast<std::size_t>(str.m_length)];
-
-        //    std::copy_n(str.m_data, m_length, m_data); // copies m_length elements of str.m_data into m_data
-
-
-        //    // return the existing object so we can chain this operator
-        //    return *this;
-        //}
-
-
-        MyString& operator=(const MyString& str)
-        {
-            std::cout << "operator=(const sMyString& str)\n";
-            MyString temp{ str };
-            swap(*this, temp);
-            return *this;
-        }
-
-        //MyString(MyString&& str) noexcept
-        //    : MyString()
-        //{
-        //    std::cout << "MyString(MyString&& str) noexcept\n";
-        //    swap(*this, str);
-        //}
-
-        friend std::ostream& operator<<(std::ostream& out, const MyString& s);
-
-        static friend void swap(MyString& first, MyString& second) // nothrow
-        {
-            std::cout << "swap\n";
-            using std::swap;
-            swap(first.m_data, second.m_data);
-            swap(first.m_length, second.m_length);
-        }
-    };
-
-
-    std::ostream& operator<<(std::ostream& out, const MyString& s)
-    {
-        return out << s.m_data;
+    case Direction::up:     return (stream << "up");
+    case Direction::down:   return (stream << "down");
+    case Direction::left:   return (stream << "left");
+    case Direction::right:  return (stream << "right");
+    default:                break;
     }
 
-    int MyString::count = 0;
-
-
+    assert(0 && "Unsupported direction was passed!");
+    return (stream << "unknown direction");
 }
 
+struct Point
+{
+    int x{};
+    int y{};
 
+    friend bool operator==(Point p1, Point p2)
+    {
+        return p1.x == p2.x && p1.y == p2.y;
+    }
 
-int main() {
-    using namespace mystring;
+    friend bool operator!=(Point p1, Point p2)
+    {
+        return !(p1 == p2);
+    }
 
-    MyString alex("Alex", 5); // Meet Alex
-    std::cout << "alex__________\n";
-    MyString employee;
-    std::cout << "employee_________\n";
-    employee = alex; // Alex is our newest employee
-    std::cout << "employee = alex___________\n";
-    std::cout << employee << '\n'; // Say your name, employee
-    std::cout << "print employee name________\n";
-    alex = alex; // Alex is himself
-    std::cout << "alex = alex_________\n";
+    Point getAdjacentPoint(Direction dir) const
+    {
+        switch (dir.getType())
+        {
+        case Direction::up:     return Point{ x,     y - 1 };
+        case Direction::down:   return Point{ x,     y + 1 };
+        case Direction::left:   return Point{ x - 1, y };
+        case Direction::right:  return Point{ x + 1, y };
+        default:                break;
+        }
 
-    std::cin.get();
+        assert(0 && "Unsupported direction was passed!");
+        return *this;
+    }
+};
+
+namespace UserInput
+{
+    bool isValidCommand(char ch)
+    {
+        return ch == 'w'
+            || ch == 'a'
+            || ch == 's'
+            || ch == 'd'
+            || ch == 'q';
+    }
+
+    void ignoreLine()
+    {
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    char getCharacter()
+    {
+        char operation{};
+        std::cin >> operation;
+        ignoreLine(); // remove any extraneous input
+        return operation;
+    }
+
+    char getCommandFromUser()
+    {
+        char ch{};
+        while (!isValidCommand(ch))
+            ch = getCharacter();
+
+        return ch;
+    }
+
+    Direction charToDirection(char ch)
+    {
+        switch (ch)
+        {
+        case 'w': return Direction{ Direction::up };
+        case 's': return Direction{ Direction::down };
+        case 'a': return Direction{ Direction::left };
+        case 'd': return Direction{ Direction::right };
+        }
+
+        assert(0 && "Unsupported direction was passed!");
+        return Direction{ Direction::up };
+    }
+};
+
+class Tile
+{
+public:
+    Tile() = default;
+    explicit Tile(int number)
+        :m_num(number)
+    {
+    }
+
+    bool isEmpty() const
+    {
+        return m_num == 0;
+    }
+
+    int getNum() const { return m_num; }
+
+private:
+    int m_num{ 0 };
+};
+
+std::ostream& operator<<(std::ostream& stream, Tile tile)
+{
+    if (tile.getNum() > 9) // if two digit number
+        stream << " " << tile.getNum() << " ";
+    else if (tile.getNum() > 0) // if one digit number
+        stream << "  " << tile.getNum() << " ";
+    else if (tile.getNum() == 0) // if empty spot
+        stream << "    ";
+    return stream;
+}
+
+class Board
+{
+public:
+
+    Board() = default;
+
+    static void printEmptyLines(int count)
+    {
+        for (int i = 0; i < count; ++i)
+            std::cout << '\n';
+    }
+
+    friend std::ostream& operator<<(std::ostream& stream, const Board& board)
+    {
+        // Before drawing always print some empty lines
+        // so that only one board appears at a time
+        // and it's always shown at the bottom of the window
+        // because console window scrolls automatically when there is no
+        // enough space.
+        for (int i = 0; i < g_consoleLines; ++i)
+            std::cout << '\n';
+
+        for (int y = 0; y < s_size; ++y)
+        {
+            for (int x = 0; x < s_size; ++x)
+                stream << board.m_tiles[y][x];
+            stream << '\n';
+        }
+
+        return stream;
+    }
+
+    Point getEmptyTilePos() const
+    {
+        for (int y = 0; y < s_size; ++y)
+            for (int x = 0; x < s_size; ++x)
+                if (m_tiles[y][x].isEmpty())
+                    return { x,y };
+
+        assert(0 && "There is no empty tile in the board!!!");
+        return { -1,-1 };
+    }
+
+    static bool isValidTilePos(Point pt)
+    {
+        return (pt.x >= 0 && pt.x < s_size)
+            && (pt.y >= 0 && pt.y < s_size);
+    }
+
+    void swapTiles(Point pt1, Point pt2)
+    {
+        std::swap(m_tiles[pt1.y][pt1.x], m_tiles[pt2.y][pt2.x]);
+    }
+
+    // Compare two boards to see if they are equal
+    friend bool operator==(const Board& f1, const Board& f2)
+    {
+        for (int y = 0; y < s_size; ++y)
+            for (int x = 0; x < s_size; ++x)
+                if (f1.m_tiles[y][x].getNum() != f2.m_tiles[y][x].getNum())
+                    return false;
+
+        return true;
+    }
+
+    // returns true if user moved successfully
+    bool moveTile(Direction dir)
+    {
+        Point emptyTile{ getEmptyTilePos() };
+        Point adj{ emptyTile.getAdjacentPoint(-dir) };
+
+        if (!isValidTilePos(adj))
+            return false;
+
+        swapTiles(adj, emptyTile);
+        return true;
+    }
+
+    bool playerWon() const
+    {
+        static Board s_solved{};  // generate a solved board
+        return s_solved == *this; // player wins if current board == solved board
+    }
+
+    void randomize()
+    {
+        // Move empty tile randomly 1000 times
+        // (just like you would do in real life)
+        for (int i = 0; i < 1000; )
+        {
+            // If we are able to successfully move a tile, count this
+            if (moveTile(Direction::getRandomDirection()))
+                ++i;
+        }
+    }
+
+private:
+    static const int s_size{ 4 };
+    Tile m_tiles[s_size][s_size]{
+        Tile{ 1 }, Tile { 2 }, Tile { 3 } , Tile { 4 },
+        Tile { 5 } , Tile { 6 }, Tile { 7 }, Tile { 8 },
+        Tile { 9 }, Tile { 10 }, Tile { 11 }, Tile { 12 },
+        Tile { 13 }, Tile { 14 }, Tile { 15 }, Tile { 0 } };
+};
+
+int main()
+{
+    Board board{};
+    board.randomize();
+    std::cout << board;
+
+    while (!board.playerWon())
+    {
+        char ch{ UserInput::getCommandFromUser() };
+
+        // Handle non-direction commands
+        if (ch == 'q')
+        {
+            std::cout << "\n\nBye!\n\n";
+            return 0;
+        }
+
+        // Handle direction commands
+        Direction dir{ UserInput::charToDirection(ch) };
+
+        bool userMoved{ board.moveTile(dir) };
+        if (userMoved)
+            std::cout << board;
+    }
+
+    std::cout << "\n\nYou won!\n\n";
     return 0;
 }
-
-/*
-
-ќператоры преобразовани¤ типов
-https://metanit.com/cpp/tutorial/5.15.php
-
-13.11 Ц ѕерегрузка операторов преобразовани¤ типов данных
-https://radioprog.ru/post/1247
-
-21.11 Ч Overloading typecasts
-https://www.learncpp.com/cpp-tutorial/overloading-typecasts/
-
-21.12 Ч Overloading the assignment operator
-https://www.learncpp.com/cpp-tutorial/overloading-the-assignment-operator/
-
-
-*/
-
-
-
-
-
-
