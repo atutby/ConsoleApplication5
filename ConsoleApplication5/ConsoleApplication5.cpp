f2d54ff2bc394806771b8f3108d9a891f471293c
Sasha HP <sasha2015to@gmail.com>
2024-08-18 00:34:16 +0300

    21.y — Chapter 21 project (done)

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -1,320 +1,306 @@
-/*
-https://www.learncpp.com/cpp-tutorial/chapter-21-project/
-21.y — Chapter 21 project
-*/
-
 #include <array>
 #include <cassert>
 #include <iostream>
 #include <limits>
 #include "Random.h"
-#include <conio.h>
 
-// Increase amount of new lines if your board isn't
-// at the very bottom of the console
+
+// Increase amount of new lines if your isn't
+// at the very bootom of the console
 constexpr int g_consoleLines{ 25 };
 
 class Direction
 {
 public:
-    enum Type
-    {
-        up,
-        down,
-        left,
-        right,
-        maxDirections,
-    };
+	enum Type
+	{
+		up,
+		down,
+		left,
+		right,
+		maxDirections,
+	};
 
-    Direction(Type type)
-        :m_type(type)
-    {
-    }
-    Type getType() const
-    {
-        return m_type;
-    }
+	Direction(Type type)
+		: m_type(type)
+	{}
 
-    Direction operator-() const
-    {
-        switch (m_type)
-        {
-        case up:    return Direction{ down };
-        case down:  return Direction{ up };
-        case left:  return Direction{ right };
-        case right: return Direction{ left };
-        default:    break;
-        }
+	Type getType() const
+	{
+		return m_type;
+	}
 
-        assert(0 && "Unsupported direction was passed!");
-        return Direction{ up };
-    }
+	Direction operator-() const
+	{
+		switch (m_type)
+		{
+		case up: return Direction{ down };
+		case down: return Direction{ up };
+		case left: return Direction{ right };
+		case right: return Direction{ left };
+		default: break;
+		}
 
-    static Direction getRandomDirection()
-    {
-        Type random{ static_cast<Type>(Random::get(0, Type::maxDirections - 1)) };
-        return Direction{ random };
-    }
+		assert(0 && "Unsupported direction was passed!");
+		return Direction{ up };
+	}
+
+	static Direction getRandomDirection()
+	{
+		Type random{ static_cast<Type>(Random::get(0, Type::maxDirections - 1)) };
+		return Direction{ random };
+	}
 
 private:
-    Type m_type{};
+	Type m_type{};
 };
 
 std::ostream& operator<<(std::ostream& stream, Direction dir)
 {
-    switch (dir.getType())
-    {
-    case Direction::up:     return (stream << "up");
-    case Direction::down:   return (stream << "down");
-    case Direction::left:   return (stream << "left");
-    case Direction::right:  return (stream << "right");
-    default:                break;
-    }
+	switch (dir.getType())
+	{
+	case Direction::up: return (stream << "up");
+	case Direction::down: return (stream << "down");
+	case Direction::left: return (stream << "left");
+	case Direction::right: return (stream << "right");
+	default: break;
+	}
 
-    assert(0 && "Unsupported direction was passed!");
-    return (stream << "unknown direction");
+	assert(0 && "Unsupported direction was passed!");
+	return (stream << "unknown direction");
 }
+
 
 struct Point
 {
-    int x{};
-    int y{};
+	int x{};
+	int y{};
 
-    friend bool operator==(Point p1, Point p2)
-    {
-        return p1.x == p2.x && p1.y == p2.y;
-    }
+	friend bool operator==(Point p1, Point p2)
+	{
+		return p1.x == p2.x && p1.y == p2.y;
+	}
 
-    friend bool operator!=(Point p1, Point p2)
-    {
-        return !(p1 == p2);
-    }
+	friend bool operator!=(Point p1, Point p2)
+	{
+		return !(p1 == p2);
+	}
 
-    Point getAdjacentPoint(Direction dir) const
-    {
-        switch (dir.getType())
-        {
-        case Direction::up:     return Point{ x,     y - 1 };
-        case Direction::down:   return Point{ x,     y + 1 };
-        case Direction::left:   return Point{ x - 1, y };
-        case Direction::right:  return Point{ x + 1, y };
-        default:                break;
-        }
+	Point getAdjacentPoint(Direction dir) const
+	{
+		switch (dir.getType())
+		{
+		case Direction::up: return Point{ x, y - 1 };
+		case Direction::down: return Point{ x, y + 1 };
+		case Direction::left: return Point{ x - 1, y };
+		case Direction::right: return Point{ x + 1, y };
+		default: break;
+		}
 
-        assert(0 && "Unsupported direction was passed!");
-        return *this;
-    }
+		assert(0 && "Unsupported direction was passed!");
+		return *this;
+	}
 };
+
+
+
 
 namespace UserInput
 {
-    bool isValidCommand(char ch)
-    {
-        return ch == 'w'
-            || ch == 'a'
-            || ch == 's'
-            || ch == 'd'
-            || ch == 'q';
-    }
+	bool isValidCommand(char ch)
+	{
+		return ch == 'w'
+			|| ch == 'a'
+			|| ch == 's'
+			|| ch == 'd'
+			|| ch == 'q';
+	}
 
-    void ignoreLine()
-    {
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    }
+	void ignoreLine()
+	{
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
 
-    char getCharacter()
-    {
-        char operation{};
-        std::cin >> operation;
-        ignoreLine(); // remove any extraneous input
-        return operation;
-    }
+	char getCharacter()
+	{
+		char operation{};
+		std::cin >> operation;
+		ignoreLine(); // remove any extraneous input
+		return operation;
+	}
 
-    char getCommandFromUser()
-    {
-        char ch{};
-        while (!isValidCommand(ch))
-            ch = getCharacter();
+	char getCommandFromUser()
+	{
+		char ch{};
+		while (!isValidCommand(ch))
+			ch = getCharacter();
 
-        return ch;
-    }
+		return ch;
+	}
 
-    Direction charToDirection(char ch)
-    {
-        switch (ch)
-        {
-        case 'w': return Direction{ Direction::up };
-        case 's': return Direction{ Direction::down };
-        case 'a': return Direction{ Direction::left };
-        case 'd': return Direction{ Direction::right };
-        }
+	Direction charToDirection(char ch)
+	{
+		switch (ch)
+		{
+		case 'w': return Direction{ Direction::up };
+		case 's': return Direction{ Direction::down };
+		case 'a': return Direction{ Direction::left };
+		case 'd': return Direction{ Direction::right };
+		}
 
-        assert(0 && "Unsupported direction was passed!");
-        return Direction{ Direction::up };
-    }
+		assert(0 && "Unsupported direction was passed!");
+		return Direction{ Direction::up };
+	}
 };
 
 class Tile
 {
 public:
-    Tile() = default;
-    explicit Tile(int number)
-        :m_num(number)
-    {
-    }
+	Tile() = default;
+	explicit Tile(int number)
+		: m_num(number)
+	{}
 
-    bool isEmpty() const
-    {
-        return m_num == 0;
-    }
+	bool isEmpty() const
+	{
+		return m_num == 0;
+	}
 
-    int getNum() const { return m_num; }
+	int getNum() const { return m_num; }
 
 private:
-    int m_num{ 0 };
+	int m_num{ 0 };
 };
 
 std::ostream& operator<<(std::ostream& stream, Tile tile)
 {
-    if (tile.getNum() > 9) // if two digit number
-        stream << " " << tile.getNum() << " ";
-    else if (tile.getNum() > 0) // if one digit number
-        stream << "  " << tile.getNum() << " ";
-    else if (tile.getNum() == 0) // if empty spot
-        stream << "    ";
-    return stream;
+	if (tile.getNum() > 9)
+		stream << " " << tile.getNum() << " ";
+	else if (tile.getNum() > 0)
+		stream << "  " << tile.getNum() << " ";
+	else if (tile.getNum() == 0)
+		stream << "    ";
+	return stream;
 }
 
 class Board
 {
 public:
+	Board() = default;
 
-    Board() = default;
+	static void printEmptyLines(int count)
+	{
+		for (int i = 0; i < count; ++i)
+			std::cout << '\n';
+	}
 
-    static void printEmptyLines(int count)
-    {
-        for (int i = 0; i < count; ++i)
-            std::cout << '\n';
-    }
+	friend std::ostream& operator<<(std::ostream& stream, const Board& board)
+	{
+		for (int i = 0; i < g_consoleLines; ++i)
+			std::cout << '\n';
 
-    friend std::ostream& operator<<(std::ostream& stream, const Board& board)
-    {
-        // Before drawing always print some empty lines
-        // so that only one board appears at a time
-        // and it's always shown at the bottom of the window
-        // because console window scrolls automatically when there is no
-        // enough space.
-        for (int i = 0; i < g_consoleLines; ++i)
-            std::cout << '\n';
+		for (int y = 0; y < s_size; ++y)
+		{
+			for (int x = 0; x < s_size; ++x)
+				stream << board.m_tiles[y][x];
+			stream << '\n';
+		}
 
-        for (int y = 0; y < s_size; ++y)
-        {
-            for (int x = 0; x < s_size; ++x)
-                stream << board.m_tiles[y][x];
-            stream << '\n';
-        }
+		return stream;
+	}
 
-        return stream;
-    }
+	Point getEmptyTilePos() const
+	{
+		for (int y = 0; y < s_size; ++y)
+			for (int x = 0; x < s_size; ++x)
+				if (m_tiles[y][x].isEmpty())
+					return { x, y };
 
-    Point getEmptyTilePos() const
-    {
-        for (int y = 0; y < s_size; ++y)
-            for (int x = 0; x < s_size; ++x)
-                if (m_tiles[y][x].isEmpty())
-                    return { x,y };
+		assert(0 && "There in no empty tile in the board!!!");
+		return { -1, -1 };
+	}
 
-        assert(0 && "There is no empty tile in the board!!!");
-        return { -1,-1 };
-    }
+	static bool isValidPos(Point pt)
+	{
+		return (pt.x >= 0 && pt.x < s_size)
+			&& (pt.y >= 0 && pt.y < s_size);
+	}
 
-    static bool isValidTilePos(Point pt)
-    {
-        return (pt.x >= 0 && pt.x < s_size)
-            && (pt.y >= 0 && pt.y < s_size);
-    }
+	void swapTiles(Point pt1, Point pt2)
+	{
+		std::swap(m_tiles[pt1.y][pt1.x], m_tiles[pt2.y][pt2.x]);
+	}
 
-    void swapTiles(Point pt1, Point pt2)
-    {
-        std::swap(m_tiles[pt1.y][pt1.x], m_tiles[pt2.y][pt2.x]);
-    }
+	// Compare two board to see if they are equal
+	friend bool operator==(const Board& f1, const Board& f2) {
+		for (int y = 0; y < s_size; ++y)
+			for (int x = 0; x < s_size; ++x)
+				if (f1.m_tiles[y][x].getNum() != f2.m_tiles[y][x].getNum())
+					return false;
 
-    // Compare two boards to see if they are equal
-    friend bool operator==(const Board& f1, const Board& f2)
-    {
-        for (int y = 0; y < s_size; ++y)
-            for (int x = 0; x < s_size; ++x)
-                if (f1.m_tiles[y][x].getNum() != f2.m_tiles[y][x].getNum())
-                    return false;
+		return true;
+	}
 
-        return true;
-    }
+	bool moveTile(Direction dir)
+	{
+		Point emptyTile{ getEmptyTilePos() };
+		Point adj{ emptyTile.getAdjacentPoint(-dir) };
 
-    // returns true if user moved successfully
-    bool moveTile(Direction dir)
-    {
-        Point emptyTile{ getEmptyTilePos() };
-        Point adj{ emptyTile.getAdjacentPoint(-dir) };
+		if (!isValidPos(adj))
+			return false;
 
-        if (!isValidTilePos(adj))
-            return false;
+		swapTiles(adj, emptyTile);
+		return true;
+	}
 
-        swapTiles(adj, emptyTile);
-        return true;
-    }
+	bool playerWon() const
+	{
+		static Board s_solved{};
+		return s_solved == *this;
+	}
 
-    bool playerWon() const
-    {
-        static Board s_solved{};  // generate a solved board
-        return s_solved == *this; // player wins if current board == solved board
-    }
-
-    void randomize()
-    {
-        // Move empty tile randomly 1000 times
-        // (just like you would do in real life)
-        for (int i = 0; i < 1000; )
-        {
-            // If we are able to successfully move a tile, count this
-            if (moveTile(Direction::getRandomDirection()))
-                ++i;
-        }
-    }
+	void randomize()
+	{
+		for (int i = 0; i < 1000;)
+		{
+			if (moveTile(Direction::getRandomDirection()))
+				++i;
+		}
+	}
 
 private:
-    static const int s_size{ 4 };
-    Tile m_tiles[s_size][s_size]{
-        Tile{ 1 }, Tile { 2 }, Tile { 3 } , Tile { 4 },
-        Tile { 5 } , Tile { 6 }, Tile { 7 }, Tile { 8 },
-        Tile { 9 }, Tile { 10 }, Tile { 11 }, Tile { 12 },
-        Tile { 13 }, Tile { 14 }, Tile { 15 }, Tile { 0 } };
+	static const int s_size{ 4 };
+	Tile m_tiles[s_size][s_size]{
+		Tile{1}, Tile{2}, Tile{3}, Tile{4},
+		Tile{5}, Tile{6}, Tile{7}, Tile{8},
+		Tile{9}, Tile{10}, Tile{11}, Tile{12},
+		Tile{13}, Tile{14}, Tile{15}, Tile{0}
+	};
 };
+
 
 int main()
 {
-    Board board{};
-    board.randomize();
-    std::cout << board;
+	Board board{};
+	board.randomize();
+	std::cout << board;
 
-    while (!board.playerWon())
-    {
-        char ch{ UserInput::getCommandFromUser() };
+	while (!board.playerWon())
+	{
+		char ch{ UserInput::getCommandFromUser() };
 
-        // Handle non-direction commands
-        if (ch == 'q')
-        {
-            std::cout << "\n\nBye!\n\n";
-            return 0;
-        }
+		// Handle non-direction commands
+		if (ch == 'q')
+		{
+			std::cout << "\n\nBye!\n\n";
+			return 0;
+		}
 
-        // Handle direction commands
-        Direction dir{ UserInput::charToDirection(ch) };
+		// Handle direction commands
+		Direction dir{ UserInput::charToDirection(ch) };
 
-        bool userMoved{ board.moveTile(dir) };
-        if (userMoved)
-            std::cout << board;
-    }
-
-    std::cout << "\n\nYou won!\n\n";
-    return 0;
+		bool userMoved{ board.moveTile(dir) };
+		if (userMoved)
+			std::cout << board;
+	}
 }
